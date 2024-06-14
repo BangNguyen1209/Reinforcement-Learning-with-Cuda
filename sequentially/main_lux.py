4d5db8861b3ca8855a0bd4d051aace9221515fb3
BangNguyen <nhhbang1209@gmail.com>
2024-06-15 00:17:51 +0700

    ok

---
+++ sequentially/main_lux.py
@@ -0,0 +1,24 @@
+from stable_baselines3 import PPO  # pip install stable-baselines3
+
+from lux.env.agent import AgentFromStdInOut
+from lux.env.lux_env import LuxEnvironment
+from lux.game.constants import LuxMatchConfigs_Default
+from agent_personal_custom import AgentPersonalCustom
+
+if __name__ == "__main__":
+    # Run a kaggle submission with the specified model
+    configs = LuxMatchConfigs_Default
+
+    # Load the saved model
+    model = PPO.load(f"model.zip")
+    
+    # Create a kaggle-remote opponent agent
+    opponent = AgentFromStdInOut()
+
+    # Create a RL agent in inference mode
+    player = AgentPersonalCustom(mode="inference", model=model)
+
+    # Run the environment
+    env = LuxEnvironment(configs, player, opponent)
+    env.reset()  # This will automatically run the game since there is
+    # no controlling learning agent.
