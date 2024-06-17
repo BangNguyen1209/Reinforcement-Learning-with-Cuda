f3e3de5a4d742a4766491e642d46fc883178f806
BangNguyen1209 <nhhbang1209@gmail.com>
2024-06-17 08:04:00 +0700

    Update PPO.py

--- Algorythm/PPO.py
+++ Algorythm/PPO.py
@@ -8,11 +8,11 @@ from torch.nn import functional as F
 from typing import Any, ClassVar, Dict, Optional, Type, TypeVar, Union
 
 #from stable_baselines3.common.on_policy_algorithm import OnPolicyAlgorithm
-#from stable_baselines3.common.policies import ActorCriticCnnPolicy, ActorCriticPolicy, BasePolicy, MultiInputActorCriticPolicy
+from stable_baselines3.common.policies import ActorCriticCnnPolicy, ActorCriticPolicy, BasePolicy, MultiInputActorCriticPolicy
 from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, Schedule
 from stable_baselines3.common.utils import explained_variance, get_schedule_fn
 
-from Algorythm.ActorCriticPolicy import ActorCriticCnnPolicy, ActorCriticPolicy, BasePolicy, MultiInputActorCriticPolicy
+#from Algorythm.ActorCriticPolicy import ActorCriticCnnPolicy, ActorCriticPolicy, BasePolicy, MultiInputActorCriticPolicy
 from Algorythm.OnPolicyAlgorithm import OnPolicyAlgorithm
 
 SelfPPO = TypeVar("SelfPPO", bound="PPO")
